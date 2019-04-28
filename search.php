0365d8fc40c851049aa72c5932f15993fb486364
mgorczyca12 <mgorczyca@stu.naperville203.org>
2019-04-27 19:40:27 -0500

    Update search.php

--- search.php
+++ search.php
@@ -2,11 +2,30 @@
 <html>
 
 	<head><title>Search</title></head>
-
+i
 	<body>
 		<?php
 			// Start the session
 			session_start();
+			
+			ini_set('display_error', 1);
+			ini_set('display_startup_errors', 1);
+			error_reporting(E_ALL);
+
+			try { // if something goes wrong, an exception is thrown
+				$dsn = 'mysql:host=courses;dbname=z1817177';
+				$username = 'z1817177';
+				$password = '1997Apr20';
+				$pdo = new PDO($dsn, $username, $password);
+			}
+
+			catch(PDOexception $e) { // handle that exception
+				echo "Connection to database failed: ";
+				echo $e->getMessage();
+				exit();
+			}
+			
+			$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 		?>
 		
 		
@@ -22,24 +41,6 @@
 				<option value="null"></option>
 				<?php
 				
-					ini_set('display_error', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
-
-					try { // if something goes wrong, an exception is thrown
-						$dsn = 'mysql:host=courses;dbname=z1817177';
-						$username = 'z1817177';
-						$password = '1997Apr20';
-						$pdo = new PDO($dsn, $username, $password);
-					}
-
-					catch(PDOexception $e) { // handle that exception
-						echo "Connection to database failed: ";
-						echo $e->getMessage();
-						exit();
-					}
-
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 					$sql = "SELECT DISTINCT AssociatedAct FROM SongCon;";
 					$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 					$success = $prepared->execute(array());
@@ -63,24 +64,6 @@
 				<option value="null"></option>
 				<?php
 				
-					ini_set('display_error', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
-
-					try { // if something goes wrong, an exception is thrown
-						$dsn = 'mysql:host=courses;dbname=z1817177';
-						$username = 'z1817177';
-						$password = '1997Apr20';
-						$pdo = new PDO($dsn, $username, $password);
-					}
-
-					catch(PDOexception $e) { // handle that exception
-						echo "Connection to database failed: ";
-						echo $e->getMessage();
-						exit();
-					}
-
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 					$sql = "SELECT DISTINCT SongTitle FROM Song;";
 					$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 					$success = $prepared->execute(array());
@@ -104,24 +87,6 @@
 				<option value="null"></option>
 				<?php
 				
-					ini_set('display_error', 1);
-					ini_set('display_startup_errors', 1);
-					error_reporting(E_ALL);
-
-					try { // if something goes wrong, an exception is thrown
-						$dsn = 'mysql:host=courses;dbname=z1817177';
-						$username = 'z1817177';
-						$password = '1997Apr20';
-						$pdo = new PDO($dsn, $username, $password);
-					}
-
-					catch(PDOexception $e) { // handle that exception
-						echo "Connection to database failed: ";
-						echo $e->getMessage();
-						exit();
-					}
-
-					$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
 					$sql = "SELECT DISTINCT ConName FROM Contributor;";
 					$prepared = $pdo->prepare($sql, array(PDO::ATTR_CURSOR => PDO::CURSOR_FWDONLY));
 					$success = $prepared->execute(array());
